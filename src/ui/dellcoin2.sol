e2a9194b1e831a612425ba8a891137adaf7363cb
andrsuh <andrsuh13@yandex.ru>
2018-01-27 11:38:01 +0300

    new contract

--- src/ui/dellcoin2.sol
+++ src/ui/dellcoin2.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.4.0;
 
 contract DellCoin {
 
-    mapping (bytes32 => int256) public serverSeedMap;
-    mapping (bytes32 => int256) public clientSeedMap;
-    mapping (bytes32 => int256) public clientOrigSumMap;
-    mapping (bytes32 => uint256) public clientCheckCountMap;
-    mapping (bytes32 => uint256) public clientFileSizeMap;
+    struct Random {
+        int32 seed;
+    }
 
-    mapping (bytes32 => address) public clients; // file -> client
-    mapping (bytes32 => address) public servers; // file -> server
+    mapping(bytes32 => int32) public serverSeedMap;
+    mapping(bytes32 => int32) public clientSeedMap;
+    mapping(bytes32 => int256) public clientOrigSumMap;
+    mapping(bytes32 => int256) public clientCheckCountMap;
+    mapping(bytes32 => int256) public clientFileSizeMap;
+
+    mapping(bytes32 => address) public clients; // file -> client
+    mapping(bytes32 => address) public servers; // file -> server
+
+    event test_value(uint256 indexed arg);
 
     address public owner;
     bytes32 private name;
@@ -33,11 +39,11 @@ contract DellCoin {
     }
 
     function() public payable {
-//        buy();
+        //        buy();
     }
 
     // клиент выложил файл в контракт
-    function addClientFile(bytes32 file, int256 clientOrigSum, int256 clientSeed, uint256 clientCheckCount, uint256 fileSize) public {
+    function addClientFile(bytes32 file, int256 clientOrigSum, int32 clientSeed, int256 clientCheckCount, int256 fileSize) public {
         clientSeedMap[file] = clientSeed;
         clientOrigSumMap[file] = clientOrigSum;
         clientCheckCountMap[file] = clientCheckCount;
@@ -47,30 +53,52 @@ contract DellCoin {
     }
 
     // сервер для файла клиента выложил ключ
-    function addServerSeed(bytes32 file, int256 serverSeed) public {
+    function addServerSeed(bytes32 file, int32 serverSeed) public {
         serverSeedMap[file] = serverSeed;
 
         servers[file] = msg.sender;
     }
 
     // вызывает клиент, если сумма сошлась - зачислить серверу деньги, клиенту отдать ключ
-    function checkValid(bytes32 file, int256 serverSum) public view returns (int256) {
-//        int256 clientSum = clientOrigSumMap[file];
+    function checkValid(bytes32 file, int256 serverSum) public returns (int32) {
+        //        int256 clientSum = clientOrigSumMap[file];
         require(serverSum > 0);
 
-//        if (clientOrigSum == serverSum - randSum) {
-//            return true;
-//        }
+        var serverRandom = Random(generateSeed(serverSeedMap[file]));
 
-//        int256 randSum = 5;
+        int32[] memory serverRandomSequence = new int32[](clientFileSizeMap[file]);
+        for (int32 i = 0; i < clientFileSizeMap[file]; i++) {
+            var sr = nextInt(serverRandom, 1000);
+            serverRandomSequence[i] = sr;
+        }
 
-//        require(clientSum == serverSum - randSum);
+        int64 randSum = 0;
+        var clientRandom = Random(generateSeed(clientSeedMap[file]));
+        for (i = 0; i < clientCheckCountMap[file]; i++) {
+            var cr = nextInt(clientRandom, int32(clientFileSizeMap[file]));
+            randSum += serverRandomSequence[uint256(cr)];
+        }
 
-        return serverSeedMap[file];
+        if (clientOrigSumMap[file] == (serverSum - randSum)) {
+            return serverSeedMap[file];
+        }
+        return - 1;
     }
 
     function kill() public onlyOwner {
         selfdestruct(owner);
     }
-}
 
+    function generateSeed(int32 seed) public returns (int32) {
+        seed = seed % 2147483647;
+        if (seed <= 0) {
+            seed += 2147483646;
+        }
+        return seed;
+    }
+
+    function nextInt(Random random, int32 range) public returns (int32) {
+        random.seed = (random.seed * 16807) % 2147483647;
+        return (random.seed - 1) % 2147483646 % range;
+    }
+}
